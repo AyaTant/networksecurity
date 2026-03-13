669fb258aa0096a479162a42cf46488611bec6d3
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-03-13 14:52:26 +0000

    script to insert data into mongodb

--- push_data.py
+++ push_data.py
@@ -1,14 +1,61 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+import os
+import sys 
+import json
+import certifi
+import pandas as pd  
+import numpy as np
+import pymongo 
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging 
+from dotenv import load_dotenv
+load_dotenv()
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
-uri = "mongodb+srv://ayatantaoui12_db_user:Admin123@cluster0.toicub7.mongodb.net/?appName=Cluster0"
+ca=certifi.where()
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def csv_to_json_converter(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+            self.mongo_client = pymongo.MongoClient(
+                                            MONGO_DB_URL,
+                                            tls=True,
+                                            tlsCAFile=certifi.where(),
+                                            serverSelectionTimeoutMS=30000)
+            self.database=self.mongo_client[self.database]
+
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__=="__main__":
+    FILE_PATH="Network_Data\phisingData.csv"
+    DATABASE="AYAAI"
+    Collection="NetworkData"
+
+    network_obj = NetworkDataExtract()
+    records=network_obj.csv_to_json_converter(file_path=FILE_PATH)
+    print(records)
+    no_of_records=network_obj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
