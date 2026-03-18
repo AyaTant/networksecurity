815be6ac6dbcf9b8210a257488a7ea9761c5e417
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-03-18 15:33:06 +0000

    adding schema file path to constant

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -13,6 +13,8 @@ FILE_NAME:str="NetworkData.csv"
 
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -31,3 +33,4 @@ DATA_VALIDATION_VALID_DIR:str= "validated"
 DATA_VALIDATION_INVALID_DIR:str= "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str= "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str= "report.yaml"
+
