ab0231adc86be42b22bb00f4eef69460a813a376
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-03-21 14:50:10 +0000

    model details added to pipeline

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,6 +15,10 @@ TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
+SAVED_MODEL_DIR = os.path.join("saved_models")
+
+MODEL_FILE_NAME="model.pkl"
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -59,3 +63,4 @@ MODEL_TRAINER_EXPECTED_SCORE:float=0.6
 MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD:float=0.05
 
 TRAINING_BUCKET_NAME ="networksecurity"
+
