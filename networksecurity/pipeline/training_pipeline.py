3cbc90f9de283806953df5b8c4a12d9a7b42e727
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-03-25 14:33:22 +0100

    training pipeline

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -81,5 +81,6 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
